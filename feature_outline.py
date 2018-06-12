af80bc5f07e2327809e56ce183f8a2b461e66bb2
Roy Chiu <rychiu13@gmail.com>
2018-06-11 19:19:00 -0500

    Places API basic search example

--- feature_outline.py
+++ feature_outline.py
@@ -1,16 +1,13 @@
 import googlemaps
 import json
-import requests
 
 # Retrieve API Key from json file
 with open('credentials.json') as keyfile:
-    key = json.load(keyfile)['api_key']
+    data = json.load(keyfile)
+    key = data['api_key']
 
 # Authenticate and connect - this only allows access to APIs that support Python client library:
 gmaps = googlemaps.Client(key=key)
-
-# APIs that require HTTP requests and endpoints
-places_endpoint = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
 
 # Geocoding API
 """
@@ -26,11 +23,14 @@ print('Geocode from coordinates to address: ' + str(reverse_geocode_ex[0]['forma
 
 # Distance Matrix API
 
+
 # Places API
-# Place Search
 """
-params = {'input': 'mcdonalds', 'inputtype': 'textquery', 'key': key}
-place_search = requests.get(places_endpoint, params=params)
-print(place_search.url)
-print(place_search.text)
+Seach for Shedd Aquarium and get all data available
+Understanding response information at https://developers.google.com/places/web-service/intro
 """
+place_search = gmaps.places('Shedd Aquarium')
+print('------------------------------------ PLACES API -----------------------------------------')
+print('Response example: ' + json.dumps(place_search, indent=4, sort_keys=False))
+print('Places API Address: ' + place_search['results'][0]['formatted_address'])
+print('Places place_id: ' + place_search['results'][0]['place_id'])
