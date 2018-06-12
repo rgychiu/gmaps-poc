a8bdf210aeab7a0fc8aa0afb217a21dbd9ce8b49
Roy Chiu <rychiu13@gmail.com>
2018-06-12 11:22:29 -0500

    Build out use cases for places API

--- feature_outline.py
+++ feature_outline.py
@@ -9,6 +9,23 @@ with open('credentials.json') as keyfile:
 # Authenticate and connect - this only allows access to APIs that support Python client library:
 gmaps = googlemaps.Client(key=key)
 
+# Places API
+"""
+Search for Shedd Aquarium and get all data available
+Understanding response information at https://developers.google.com/places/web-service/intro
+"""
+place_search = gmaps.places('Shedd Aquarium')
+place_details = gmaps.place(place_search['results'][0]['place_id'])
+place_suggestions = gmaps.places_autocomplete('Paris')
+place_suggest_query = gmaps.places_autocomplete_query('pizza near Chicago')
+print('------------------------------------ PLACES API -----------------------------------------')
+print('Response example: ' + json.dumps(place_search, indent=4, sort_keys=False))
+print('Places API Address: ' + place_search['results'][0]['formatted_address'])
+print('Places place_id: ' + place_search['results'][0]['place_id'])
+print('Place details example response: ' + json.dumps(place_details, indent=4, sort_keys=False))
+print('Place suggestions example response: ' + json.dumps(place_suggestions, indent=4, sort_keys=False))
+print('Place query suggestions example response: ' + json.dumps(place_suggest_query, indent=4, sort_keys=False))
+
 # Geocoding API
 """
 Convert address into coordinates and coordinates (latitude, longitude) to address
@@ -24,6 +41,7 @@ print('Geocode from coordinates to address: ' + str(reverse_geocode_ex[0]['forma
 # Distance Matrix API
 """
 Get distance between two cities (San Antonio and Orlando) in both miles and kilometers.
+Understanding response information at https://developers.google.com/maps/documentation/distance-matrix/intro
 """
 address_distance = gmaps.distance_matrix("San Antonio, TX", "Orlando, FL")
 address_dist_imperial = gmaps.distance_matrix("San Antonio, TX", "Orlando, FL", units="imperial")
@@ -32,14 +50,3 @@ print('------------------------------------ DISTANCE MATRIX API ----------------
 print('Response example: ' + json.dumps(address_dist_imperial, indent=4, sort_keys=False))
 print('Distance in metric units: ' + address_distance['rows'][0]['elements'][0]['distance']['text'])
 print('Distance in imperial units: ' + address_dist_imperial['rows'][0]['elements'][0]['distance']['text'])
-
-# Places API
-"""
-Search for Shedd Aquarium and get all data available
-Understanding response information at https://developers.google.com/places/web-service/intro
-"""
-place_search = gmaps.places('Shedd Aquarium')
-print('------------------------------------ PLACES API -----------------------------------------')
-print('Response example: ' + json.dumps(place_search, indent=4, sort_keys=False))
-print('Places API Address: ' + place_search['results'][0]['formatted_address'])
-print('Places place_id: ' + place_search['results'][0]['place_id'])
